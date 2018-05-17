18a2dcc8ecb654d47e5f966a27e746e154ae6275
Hong Chen <chen420@gmail.com>
2018-05-18 03:02:19 +0900

    ESP.deepSleep when failed to connect WiFi

--- TinyWebDB-WeatherStation.ino
+++ TinyWebDB-WeatherStation.ino
@@ -77,7 +77,8 @@ void setup() {
     if(!wifiManager.autoConnect()) {
       Serial.println("failed to connect and hit timeout");
       //reset and try again, or maybe put it to deep sleep
-      ESP.reset();
+      // ESP.reset();
+      ESP.deepSleep(300 * 1000 * 1000 , WAKE_RF_DEFAULT);
       delay(1000);
     } 
   
